192adb74881f652785019d9f4732fccc6d51145a
Shekhar-3010 <98082889+Shekhar-3010@users.noreply.github.com>
2023-04-03 00:10:44 +0530

    ADD:navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbarstyles.css"
 import { Link } from 'react-router-dom'
+import {FaBars,FaTimes} from "react-icons/fa";
 
 const Navbar = () => {
+    const[click,setClick] =useState(false);
+    const handleCLicking = () =>setClick(!click)
+
+    
   return (
     <div className='header'>
     <Link to="/">
-    <h1>Portfolio</h1>
+    <h1>Shashishekhar</h1>
     </Link>
+    <ul className={click ? "nav-menu active":"nav-menu"}>
+        <li>
+        <Link to="/">Home</Link>
+        </li>
+        <li>
+        <Link to="/Project">Project</Link>
+        </li>
+        <li>
+        <Link to="/About">About</Link>
+        </li>
+        <li>
+        <Link to="/contact">Contact</Link>
+        </li>
+    </ul>
+    <div className='HamBurger' onClick={handleCLicking }>
+        {click ? (  <FaBars size={20} style={{color:"#fff"}}/>):(<FaTimes size={20} style={{color:"#fff"}}/>
+        ) }
+        
+
+    </div>
       
     </div>
   )
