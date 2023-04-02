3ba2f9a1f8cdd4cdbe5ce034ac0292a7ceff2271
Shekhar-3010 <98082889+Shekhar-3010@users.noreply.github.com>
2023-04-02 23:02:58 +0530

    ADD:project created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import Home from "./Routes/Home";
+import Contact from "./Routes/contact";
+import About from "./Routes/About";
+import Projects from "./Routes/Projects";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Routes>
+    <Route path="/" element={<Home/>}/ > 
+    <Route path="/contact" element={<contact/>}/ > 
+    <Route path="/Projects" element={<Projects/>}/ > 
+    <Route path="/About" element={<About/>}/ > 
+    </Routes>
+   </>
   );
 }
 
---
+++ src/Routes/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      About ju
+    </div>
+  )
+}
+
+export default About
